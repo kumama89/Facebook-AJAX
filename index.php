b820af90e1650b85d84b27aa58515d3d04bf0042
Adam Azad <adamzazad@yahoo.com>
2014-06-16 12:11:27 +0300

    committed files

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <title>Facebook - Login W/ AJAX</title>
+          <link rel="stylesheet" href="style.css"/>
+    </head>
+     <body>
+        <div class="container">
+
+            <h1><a href="">Facebook - Login W/ AJAX</a></h1>
+            
+        <?php
+        if ( isset($_SESSION['fb-user']) ) {
+            $user = $_SESSION['fb-user'];
+        ?>
+            <div>
+               <p>Hey, Good to see you <a href="<?php echo $user['link'] ?>"><?php echo $user['first_name']; ?></a>!</p>
+            </div> 
+        <?php
+            } else {
+        ?>
+            <div>
+               <p>Hey there, Login with your Facebook</p>
+            </div>
+            <a class="fsl fsl-facebook" href="#" id="login">
+               <span class="fa-facebook fa-icons fa-lg"></span>
+               <span class="sc-label">Login W/ Facebook</span>
+            </a>    
+
+        <?php  
+        }
+        ?>              
+        </div>
+         <div id="fb-root"></div>
+         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+         <script src="script.js"></script>
+    </body>
+</html>
---
+++ set-session.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'facebook-sdk/facebook.php';
+header('Content-Type: application/json');
+
+if ( isset($_GET['accessToken']) && !empty($_GET['accessToken']) ) {
+
+        $accessToken = $_GET['accessToken'];
+        $facebook    = new Facebook(array(
+                                    'appId'  => '{Your-app-id}',
+                                    'secret' => '{Your-app-secret}',
+                                    'cookie' => true
+        ));
+        
+        # set the access_token for $facebook for future calls
+        $facebook->setAccessToken($accessToken);
+
+        try {
+
+             $userData = $facebook->api('v2.0/me');
+             # register a new session for the user, containing their basic information, id, username, first_name, last_name, profile_picture
+             # and another one for the access_token.
+             $_SESSION['fb-user-token'] = $accessToken;
+             $_SESSION['fb-user']       = $userData;
+
+             echo json_encode( array('id'         => $userData['id'],
+             	                     'link'       => $userData['link'],
+             	                     'username'   => $userData['uesrname'],
+             	                     'first_name' => $userData['first_name'],
+             	                     'last_name'  => $userData['last_name'],
+             	                     'gender'     => $userData['gender']      ), JSON_PRETTY_PRINT);
+
+        } catch (FacebookApiException $e) {
+
+           error_log($e);
+
+	    }
+
+} else {
+   header('HTTP/1.1 400');
+}
